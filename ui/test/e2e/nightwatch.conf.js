1d057ab6642f89dcb44cf34cd44b98f0ae9e8190
Shane Mook <shane.mook@gmail.com>
2017-03-03 13:21:29 -0600

    Base Vue.js Webpack scaffolding with minor corrections to make the test pass and server boot.

---
+++ ui/test/e2e/nightwatch.conf.js
@@ -0,0 +1,41 @@
+require('babel-register')
+var config = require('../../config')
+
+// http://nightwatchjs.org/guide#settings-file
+module.exports = {
+  src_folders: ['test/e2e/specs'],
+  output_folder: 'test/e2e/reports',
+  custom_assertions_path: ['test/e2e/custom-assertions'],
+
+  selenium: {
+    start_process: true,
+    server_path: require('selenium-server').path,
+    host: '127.0.0.1',
+    port: 4444,
+    cli_args: {
+      'webdriver.chrome.driver': require('chromedriver').path
+    }
+  },
+
+  test_settings: {
+    default: {
+      selenium_port: 4444,
+      selenium_host: 'localhost',
+      silent: true,
+      globals: {
+        devServerURL: 'http://localhost:' + (process.env.PORT || config.dev.port)
+      }
+    },
+
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+        binary: "/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
+        },
+        javascriptEnabled: true,
+        acceptSslCerts: true
+      }
+    }
+  }
+}
