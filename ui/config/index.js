c026014bf87bd12772544d9e83b318309d7b97cb
Shane Mook <shane.mook@gmail.com>
2017-03-03 21:16:18 -0600

    Initial spike with ELEMENT UI and not ui-kit..  Things are working.

--- ui/config/index.js
+++ ui/config/index.js
@@ -24,7 +24,7 @@ module.exports = {
   dev: {
     env: require('./dev.env'),
     port: 8080,
-    autoOpenBrowser: true,
+    autoOpenBrowser: false,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
@@ -33,6 +33,6 @@ module.exports = {
     // (https://github.com/webpack/css-loader#sourcemaps)
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
-    cssSourceMap: false
+    cssSourceMap: true
   }
 }
--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,23 +1,34 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+  <el-row id="app" :gutter="20">
+    <el-col :span="6">
+      <div class="grid-content bg-purple">
+        <h3>Nav Pop Over</h3>
+        <p>Pop over nav side column</p>
+      </div>
+    </el-col>
+
+    <el-col :span="18">
+      <div class="grid-content bg-purple">
+        <h1>Main Content Here</h1>
+        <router-view></router-view>
+      </div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-export default {
-  name: 'app',
-};
+ export default {
+   name: 'app',
+ };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ /* #app { */
+ /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
+ /* -webkit-font-smoothing: antialiased; */
+ /* -moz-osx-font-smoothing: grayscale; */
+ /* text-align: center; */
+ /* color: #2c3e50; */
+ /* margin-top: 60px; */
+ /* } */
 </style>
